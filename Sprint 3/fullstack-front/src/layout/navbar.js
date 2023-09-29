cc3e2683c494eeb38ce8fdf56858bf7ab6b0e131
Oscar Miranda Puentes <110064371+OscarMirandaPuentes@users.noreply.github.com>
2023-09-28 22:09:48 -0500

    Add files via upload
    Cambio en el navbar para que sea más funcional.

--- Sprint 3/fullstack-front/src/layout/navbar.js
+++ Sprint 3/fullstack-front/src/layout/navbar.js
@@ -1,28 +1,54 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
-export default function navbar() {
-  return (
-    <div>
-     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
-            Seguridad Chapinero USA
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span> 
-          </button>
-          <Link className="btn btn-outline-light" to="/admin">Administrador</Link>
-        </div>
-      </nav>
-    </div>
-  )
+ 
+export const Navbar = () => {
+    return (
+            <nav class="navbar fixed-top navbar-expand-md navbar-dark bg-dark mb-3">
+                <div class="flex-row d-flex">
+                    <button type="button" class="navbar-toggler mr-2 " data-toggle="offcanvas" title="Toggle responsive left sidebar">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <a class="navbar-brand" href="#" title="Ingeniería de Software">Seguridad Chapinero USA</a>
+                </div>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="navbar-collapse collapse" id="collapsingNavbar">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                          <Link className="nav-link" to="/">
+                              Home
+                          </Link>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="//www.codeply.com">Login</a>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/admin">Admin</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="" data-target="#myModal" data-toggle="modal">About</a>
+                        </li>
+                        <li class="nav-item">
+                  <a class="nav-link waves-effect waves-light text-white">
+                    <i class="fab fa-google-plus-g"></i>
+                  </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link waves-effect waves-light text-white">
+                        <i class="fas fa-envelope-open-text"></i>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link waves-effect waves-light text-white">
+                          <i class="fas fa-align-justify"></i>
+                      </a>
+                    </li>
+                    </ul>
+                </div>
+       </nav>
+    )
 }
+export default Navbar
