7628522df4a2cb4d8bc190c85522c39826f4cb1b
Oscar Miranda Puentes <110064371+OscarMirandaPuentes@users.noreply.github.com>
2023-09-30 20:47:58 -0500

    Add files via upload
    Cambio en la interfaz de userRepository.

--- Sprint 3/fullstack-backend/src/main/java/com/oscar/fullstackbackend/repository/UserRepository.java
+++ Sprint 3/fullstack-backend/src/main/java/com/oscar/fullstackbackend/repository/UserRepository.java
@@ -4,5 +4,5 @@ import com.oscar.fullstackbackend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
-
+    long count();
 }
