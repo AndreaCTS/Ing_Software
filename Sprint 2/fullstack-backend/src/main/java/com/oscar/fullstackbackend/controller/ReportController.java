d7afce3a01db3971a7b93d0512929bbcc7568723
Carlos Bermudez <carlosbermudezgalvan132@gmail.com>
2023-10-01 15:33:09 -0500

    Update controller

--- Sprint 2/fullstack-backend/src/main/java/com/oscar/fullstackbackend/controller/ReportController.java
+++ Sprint 2/fullstack-backend/src/main/java/com/oscar/fullstackbackend/controller/ReportController.java
@@ -3,9 +3,12 @@ package com.oscar.fullstackbackend.controller;
 import com.oscar.fullstackbackend.model.Report;
 import com.oscar.fullstackbackend.repository.ReportRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ReportController {
@@ -16,4 +19,11 @@ public class ReportController {
     Report newReport(@RequestBody Report newReport) {
         return reportRepository.save(newReport);
     }
+
+    @GetMapping("/reports")
+    List<Report> getAllReports(){
+        return reportRepository.findAll();
+    }
+
+
 }
