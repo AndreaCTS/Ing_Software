90251831f33b7102c4191975aeb87f6edec94ff7
Miguel Thomas <miguel.thomas01@usa.edu.co>
2023-10-15 10:47:46 -0500

    Sprint 4

--- Sprint 2/fullstack-backend/src/main/java/com/oscar/fullstackbackend/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.oscar.fullstackbackend.repository;
-
-import com.oscar.fullstackbackend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-
-}
