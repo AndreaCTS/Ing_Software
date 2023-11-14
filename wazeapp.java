a4925f50c06895fef7a683a01726d9ca61bcb273
lauramoreno23 <142845666+lauramoreno23@users.noreply.github.com>
2023-11-13 23:49:24 -0500

    Create wazeapp.java

---
+++ wazeapp.java
@@ -0,0 +1,11 @@
+package com.tuempresa.wazeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WazeAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WazeAppApplication.class, args);
+    }
+}
