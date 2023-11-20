6f5407b1c023041ec8d24e03bbee5b960600aad4
Carlos Bermudez <106134881+carlosbermudezgalvan@users.noreply.github.com>
2023-11-20 02:30:44 +0100

    blank front for reports

---
+++ fullstack-front/src/App.js
@@ -0,0 +1,56 @@
+import './styles/App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import CustomNavbar from './layout/navbar';
+import { QueryClient,QueryClientProvider } from 'react-query';
+import HomeInit from './pages/homeinit';
+import Admin from './pages/admin';
+import{BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AddUser from './users/AddUser';
+import EditUser from './users/EditUser';
+import ViewUser from "./users/ViewUser";
+import ViewComment from './comments/ViewComment';
+import AddComment from "./comments/AddComment";
+import DeleteComment from "./comments/DeleteComment";
+import Login from './users/Login';
+import 'font-awesome/css/font-awesome.css';
+import Dashboard from './pages/dashboard';
+import ViewWheel from './wheels/ViewWheel';
+import AddWheel from './wheels/AddWheel';
+import DeleteWheels from './wheels/DeleteWheel';
+import NewInit from './pages/newInit';
+import MapView from './component/MapView'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const queryClient = new QueryClient();
+
+function App() {
+  
+  return (
+    <div className="App">
+        <QueryClientProvider client={queryClient}>
+      <Router>
+        <CustomNavbar/>
+        <Routes>
+          <Route exact path="/" element={<NewInit />} />
+          <Route exact path='/mainmenu' element={<HomeInit/>}/>
+          <Route exact path="/admin" element={<Admin />} />
+          <Route exact path="/mapa" element={<MapView />} />
+          <Route exact path="/adduser" element={<AddUser />} />
+          <Route exact path='/dashboard' Component={Dashboard}/>
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/viewwheels" element={<ViewWheel />} />
+          <Route exact path="/addwheels" element={<AddWheel />} />
+          <Route exact path='/edituser/:id' element={<EditUser/>}/>
+          <Route exact path="/viewuser/:id" element={<ViewUser />} />
+          <Route exact path="/viewcomments" element={<ViewComment />} />
+          <Route exact path="/addcomments" element={<AddComment/>} />
+          <Route exact path="/deletecomments" element={<DeleteComment />} />
+          <Route exact path="/deletewheels" element={<DeleteWheels/>} />
+        </Routes>
+      </Router>
+      </QueryClientProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ fullstack-front/src/users/AddUser.js
@@ -0,0 +1,130 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+export default function AddUser() {
+  let navigate = useNavigate();
+
+  const [user, setUser] = useState({
+    name: "",
+    username: "",
+    email: "",
+    password: "",
+    role: "USER",
+  });
+
+  const [error, setError] = useState("");
+
+  const { name, username, email, password, role } = user;
+
+  const onInputChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+    setError(""); // Clear any previous errors when input changes
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:8080/userAuth/register", user);
+      navigate("/login");
+    } catch (err) {
+      if (err.response && err.response.status === 400) {
+        setError("User with this email or username already exists.");
+      } else {
+        setError("An error occurred while processing your request.");
+      }
+    }
+  };
+
+  const handleChange = (event) => {
+    const selectedValue = event.target.value;
+    setUser({ ...user, role: selectedValue });
+  };
+
+  return (   
+    <div  className="d-flex justify-content-center align-items-center" style={{ height: "100vh", background: "linear-gradient(190deg, rgba(34,193,195,0.2539216370141807) 0%, rgba(253,187,45,0.2539216370141807) 100%)" }}>
+        <div className="bg-light col-md-6  border border-dark rounded p-4 mt-5  shadow-lg  "  >
+          <h2 className="text-center m-4">Register User</h2>
+          {error && (
+            <div className="alert alert-danger" role="alert">
+              {error}
+            </div>
+          )}
+
+          <form onSubmit={(e) => onSubmit(e)}>
+            <div className="mb-3">
+              <label htmlFor="Name" className="form-label">
+                Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter your name"
+                name="name"
+                value={name}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="Username" className="form-label">
+                Username
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter your username"
+                name="username"
+                value={username}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="Email" className="form-label">
+                E-mail
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter your e-mail address"
+                name="email"
+                value={email}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="Password" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Enter your password"
+                name="password"
+                value={password}
+                onChange={(e) => onInputChange(e)} 
+              />
+            </div>
+            {/*
+            <div className="mb-3">
+              <label htmlFor="Role" className="form-label">
+                Role
+              </label>
+              <select name="role" value={role}  onChange={handleChange}>
+                  <option value="USER">User</option>
+                  <option value="ADMIN">Admin</option>
+              </select>
+            </div>
+          */}
+            <button type="submit" className="btn btn-outline-primary">
+              Submit
+            </button>
+            <Link className="btn btn-outline-danger mx-2" to="/">
+              Cancel
+            </Link>
+          </form>
+        </div>
+        </div>
+     
+    
+  );
+}
