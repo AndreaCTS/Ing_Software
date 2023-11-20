95a8dca0ca9a1110c4207b0ad672683d8bcc8597
Carlos Bermudez <106134881+carlosbermudezgalvan@users.noreply.github.com>
2023-11-19 20:13:08 -0500

    Refactor Login OAUTH2

--- fullstack-backend/src/main/java/com/oscar/fullstackbackend/exception/UserNotExistException.java
+++
@@ -1,7 +0,0 @@
-package com.oscar.fullstackbackend.exception;
-
-public class UserNotExistException extends RuntimeException{
-    public UserNotExistException(String message) {
-        super(message);
-    }
-}
