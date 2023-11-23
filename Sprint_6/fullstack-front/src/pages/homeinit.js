175c03c7e9213ee3a56f1c618a9afe719990b356
AndreaCTS <andreats0729@gmail.com>
2023-11-22 23:16:40 -0500

    new interface user menu

--- Sprint_6/fullstack-front/src/pages/homeinit.js
+++ Sprint_6/fullstack-front/src/pages/homeinit.js
@@ -1,16 +1,10 @@
 import React, { useState } from 'react';
 import '../styles/estilos.css';
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Link, useNavigate } from 'react-router-dom';
 
 export default function HomeInit () {
   const [selectedSlide, setSelectedSlide] = useState(0);
   const navigate = useNavigate();
-
-  const handleSlideChange = (index) => {
-    setSelectedSlide(index);
-  };
 
   const OnClickImage = (index) =>{
     if(index===1){
@@ -23,76 +17,22 @@ export default function HomeInit () {
       navigate("/viewwheels")
     }
   }
-  
-  const customArrowStyles = {
-    position: 'absolute',
-    zIndex: 2,
-    top: '50%',
-    transform: 'translateY(-50%)',
-    cursor: 'pointer',
-  };
-  
-  function getBackgroundColor(index) {
-    // Define los gradientes según el índice de la diapositiva seleccionada
-    const gradients = [
-      'linear-gradient(190deg, rgba(158,80,199,1) 0%, rgba(139,201,255,0.3575630935968137) 100%)',
-      'linear-gradient(190deg, rgba(235,127,127,1) 0%, rgba(149,88,186,0.3575630935968137) 100%)',
-      'linear-gradient(190deg, rgba(61,93,154,1) 0%, rgba(67,186,88,0.3575630935968137) 100%)',
-      'linear-gradient(190deg, rgba(0,0,255,1) 0%, rgba(0,255,255,1) 100%)',
-    ];
-  
-    return gradients[index] || 'linear-gradient(190deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 100%)';
-    // Fallback al gradiente blanco si el índice no coincide
-  }
 
   return (
-    <div className="initial" style={{ background: getBackgroundColor(selectedSlide) }}>
+    <div style={{backgroundColor:'white'}} >
       <header></header>
-      <main className="center-main">
-        <h1 style={{ marginBottom: '5px', color:"white" }}>
-          <em>Secciones</em>
-        </h1>
+      <main>
         <section>
-          <Carousel
-            showThumbs={false}
-            showStatus={false}
-            emulateTouch
-            selectedItem={selectedSlide}
-            onChange={handleSlideChange}
-            renderArrowPrev={(onClickHandler, hasPrev, label) =>
-              hasPrev && (
-                <button
-                  type="button"
-                  onClick={onClickHandler}
-                  title={label}
-                  style={{ ...customArrowStyles, left: 0 }}
-                >
-                  &#9664;
-                </button>
-              )
-            }
-            renderArrowNext={(onClickHandler, hasNext, label) =>
-              hasNext && (
-                <button
-                  type="button"
-                  onClick={onClickHandler}
-                  title={label}
-                  style={{ ...customArrowStyles, right: 0 }}
-                >
-                  &#9654;
-                </button>
-              )
-            }
-          >
-            <div className="breedCard">
+          <ul className="breedCardContainer">
+            <li className="breedCard">
               <div className="contenedorImagen" onClick={() => OnClickImage(1)} >
                 <img src="../media/map.jpg" alt="Risk map" />
               </div>
               <h4 >Mapa</h4>
-              <Link to="/mapa" className="btn btn-light btn-custom">
+              <Link to="/mapa" className="btn btn-dark btn-custom">
                 Ver más
               </Link>
-            </div>
+            </li>
             {/*
             <div className="breedCard">
               <div className="contenedorImagen" onClick={() => OnClickImage(2)}>
@@ -104,28 +44,27 @@ export default function HomeInit () {
               </Link>
             </div>
           */}
-            <div className="breedCard">
+            <li className="breedCard">
               <div className="contenedorImagen" onClick={() => OnClickImage(3)}>
                 <img src="../media/blog.jpg" alt="Forum" />
               </div>
               <h4 >Foro</h4> 
-              <Link to="/viewcomments" className="btn btn-light">
+              <Link to="/viewcomments" className="btn btn-dark">
                 Ver más
               </Link>
-            </div>
-            <div className="breedCard">
+            </li>
+            <li className="breedCard">
               <div className="contenedorImagen" onClick={() => OnClickImage(4)}> 
                 <img src="../media/wheel.jpg" alt="Wheel" />
               </div>
               <h4 >Wheels</h4> 
-              <Link to="/viewwheels" className="btn btn-light">
+              <Link to="/viewwheels" className="btn btn-dark">
                 Ver más
               </Link>
-            </div>
-          </Carousel>
+            </li>
+          </ul>
         </section>
       </main>
     </div>
   );
 }
-
