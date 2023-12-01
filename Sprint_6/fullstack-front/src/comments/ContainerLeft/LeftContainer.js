adcfb5a3ec6048d790dd51de444dea445f518f5b
Carlos Bermudez <106134881+carlosbermudezgalvan@users.noreply.github.com>
2023-12-01 05:30:16 -0500

    Delete Sprint_6 directory

--- Sprint_6/fullstack-front/src/comments/ContainerLeft/LeftContainer.js
+++
@@ -1,9 +0,0 @@
-import './leftcontainer.css';
-export default function LeftContainer(){
-
-    return(
-        <div className='left-container'>
-            <h2>HOLAAAA</h2>
-        </div>
-    );
-}
