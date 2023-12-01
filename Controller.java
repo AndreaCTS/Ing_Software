3321f1f036bcde27f654444553c9b11a97cb109a
unknown <lauracamila.moreno01@usa.edu.co>
2023-11-30 21:02:58 -0500

    Subir Los archivos por cambios

--- Controller.java
+++ Controller.java
@@ -1,24 +1,19 @@
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping("/api/map")
 public class MapController {
 
-    @GetMapping("/map")
-    public String showMap(Model model) {
-        // Simula una lista de coordenadas de ruta
-        List<Coordinate> routeCoordinates = new ArrayList<>();
-        routeCoordinates.add(new Coordinate(37.7749, -122.4194)); // San Francisco
-        routeCoordinates.add(new Coordinate(34.0522, -118.2437)); // Los Angeles
-        routeCoordinates.add(new Coordinate(41.8781, -87.6298)); // Chicago
-
-        model.addAttribute("routeCoordinates", routeCoordinates);
-
-        return "map";
+    @GetMapping("/coordinates")
+    public List<Coordinate> getRouteCoordinates() {
+        return Arrays.asList(
+                new Coordinate(4.710989, -74.072090), // Bogotá
+  
+        );
     }
 }
